56218a08670f6aaea91d88e5fd4595eabb6009d7
XKoutaX <carolsfartz@gmail.com>
2020-02-17 09:18:20 +0200

    comment

--- app/src/main/java/com/example/mydaggertest2/di/AppComponent.java
+++ app/src/main/java/com/example/mydaggertest2/di/AppComponent.java
@@ -7,7 +7,7 @@ import javax.inject.Singleton;
 import dagger.Component;
 
 
-// we want this Component to provide the objects that are scopes with @Singleton ( just Driver ),
+// we want this Component to provide the objects that are scoped with @Singleton ( just Driver ),
 // because we want to get this object from our AppComponent, and NOT from our ActivityComponent (which has @PerActivity, and not @Singleton)
 @Singleton
 @Component(modules = DriverModule.class)
